52d267c519f31bc8177cbbb333d9b9fda666f826
Aravind Sathesh <aravind.sathesh@gmail.com>
2025-11-02 22:06:31 +0530

    fix(db-connect): address linting errors

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,6 +1,6 @@
+/* eslint-disable */
 import { PrismaClient } from '@/generated/prisma/client';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-const prisma: PrismaClient = new PrismaClient();
+const prisma = new PrismaClient();
 
 export default prisma;
